47f5b750ab13d5dcfbcf9da2940778bb29d777da
Chandan N <cnagaraj@deloitte.com>
2022-11-29 12:18:02 +0530

    Initial Version

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+console.log(`
+
+N Chandan - Software Engineer
+
+Enterprise Application design and Development on Microsoft Azure, Database Developer in a wide variety of business applications for large Enterprise customers . Particularly interested in cloud based designs and solutions.
+
+Experience in open stack frameworks like MongoDB/Angular Frameworks/Knockout/D3.Js etc
+
+Certification
+ -Microsoft Certified Professional
+
+Specialties: Azure design principles ,Front door service, ARM templates, MongoDB,.NetCore;.Net Framework,MVC,Entity Framework, REST Services,Angular,MS SQL Server ,Web Services/WCF ,Design Patterns
+
+`);
